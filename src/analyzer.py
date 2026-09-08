01986f524cf00e305f2805d07de2ea1ef0993518
Hashani Pamodhya <hashanitharu@gmail.com>
2026-09-08 22:30:26 +0530

    Stage 3: extract username and IP from failed login lines

--- src/analyzer.py
+++ src/analyzer.py
@@ -5,4 +5,7 @@ log_file = "../logs/sample_auth.log"
 with open(log_file, "r") as file:
     for line in file:
         if "Failed password" in line:
-            print(line.strip())
+            words = line.split()
+            username = words[8]   # the word after "for"
+            ip_address = words[10]  # the word after "from"
+            print(f"User: {username} | IP: {ip_address}")
